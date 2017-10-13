47438b76eece3ed496f4dc34649e820ef23601f8
Panos <phpanos@gmail.com>
2017-10-13 16:13:50 +0200

    set application lang to swedish and translated some password reset sentences

---
+++ resources/lang/sv/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Lösenordet måste vara minst 6 tecken och matcha bekräftelselösenordet.',
+    'reset' => 'Ditt lösenord har nu blivit återställt!',
+    'sent' => 'Vi har nu mailat en länk för att återställa ditt lösenord!',
+    'token' => 'Din återställningstoken är ej giltig.',
+    'user' => "Vi kan inte hitta någon användare med den e-postadressen.",
+
+];

b1513e2736c32cb87125e93d40f49071dc527f5e
Shift <shift@laravelshift.com>
2022-07-25 11:53:38 +0000

    Shift config files

--- config/app.php
+++ config/app.php
@@ -166,7 +166,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Laravel\Tinker\TinkerServiceProvider::class,
         GrahamCampbell\Exceptions\ExceptionsServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
 
